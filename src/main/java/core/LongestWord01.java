198c66f90b133574709cb1f019ccc5b4a63529de
TonyYuta <tonykagaya@gmail.com>
2016-01-30 15:01:33 -0800

    modify name of array

--- src/main/java/core/LongestWord01.java
+++ src/main/java/core/LongestWord01.java
@@ -3,10 +3,10 @@ package core;
 public class LongestWord01 {
 	public static void main(String[] args) {
 		String myString = "apple banana pear orange peack greap strawberry plum apricot melon watermelon";
-		String[] a = myString.split(" ");
+		String[] words = myString.split(" ");
 		int maxLength = 0;
 		String longestWord = null;
-		for (String s : a) {
+		for (String s : words) {
 			if (s.length() > maxLength) {
 				maxLength = s.length();
 				longestWord = s;
